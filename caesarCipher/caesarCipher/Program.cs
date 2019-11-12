415802081704e04cc199e63427d115069f96fb12
07KenzoSenior <07KenzoSenior@IOMG21682W>
2019-11-12 15:08:23 +0000

    caesar done

--- caesarCipher/caesarCipher/Program.cs
+++ caesarCipher/caesarCipher/Program.cs
@@ -13,15 +13,61 @@ namespace caesarCipher
             string message = encrpyt("Cheese please!", 15);
             Console.WriteLine(message);
             Console.WriteLine(Decrypt(message, 15));
+            Console.ReadKey();
         }
 
-        static string encrpyt (string plaintext, int shiftKey)
+        static string encrpyt(string plaintext, int shiftKey)
         {
-            string tempChar;
-            int currentASCII;
             string cipherText = "";
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                int ASCII = (int)plaintext[i];
+                if (char.IsUpper(plaintext[i]))
+                {
+                    ASCII = ASCII - 65;
+                    ASCII = ASCII + shiftKey;
+                    ASCII = ASCII % 26;
+                    ASCII = ASCII + 65;
+                }
+                else if (char.IsLower(plaintext[i]))
+                {
+                    ASCII = ASCII - 97;
+                    ASCII = ASCII + shiftKey;
+                    ASCII = ASCII + 26;
+                    ASCII = ASCII % 26;
+                    ASCII = ASCII + 97;
 
+                }
+                cipherText = cipherText + (char)ASCII;
+            }
             return cipherText;
+        }
+
+        static string Decrypt(string cipherText, int shiftKey)
+        {
+            string plaintext = "";
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int ASCII = (int)cipherText[i];
+                if (char.IsUpper(cipherText[i]))
+                {
+                    ASCII = ASCII - 65;
+                    ASCII = ASCII - shiftKey;
+                    ASCII = ASCII % 26;
+                    ASCII = ASCII + 65;
+                }
+                else if (char.IsLower(cipherText[i]))
+                {
+                    ASCII = ASCII - 97;
+                    ASCII = ASCII - shiftKey;
+                    ASCII = ASCII + 26;
+                    ASCII = ASCII % 26;
+                    ASCII = ASCII + 97;
+
+                }
+                plaintext = plaintext + (char)ASCII;
+            }
+            return plaintext;
         }
     }
 }
