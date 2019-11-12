96303cbe6809bfd19966fb46e4564281f47476c2
07KenzoSenior <07KenzoSenior@IOMG21682W>
2019-11-12 14:29:29 +0000

    Caesar

---
+++ caesarCipher/caesarCipher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = encrpyt("Cheese please!", 15);
+            Console.WriteLine(message);
+            Console.WriteLine(Decrypt(message, 15));
+        }
+
+        static string encrpyt (string plaintext, int shiftKey)
+        {
+            string tempChar;
+            int currentASCII;
+            string cipherText = "";
+
+            return cipherText;
+        }
+    }
+}
