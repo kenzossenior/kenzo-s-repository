3f9a2fcc15073d111126bb68d0292b625da11a94
07KenzoSenior <07KenzoSenior@IOMG21682W>
2019-11-19 14:16:06 +0000

    File

---
+++ filename/filename/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace writingToAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"lines.txt";
+
+            using (StreamWriter CurrentFile = new StreamWriter(filename))
+            {
+                for (int i = 0; i == 9; i++)
+                {
+                    CurrentFile.WriteLine(i);
+                }
+            }
+        }
+    }
+}
