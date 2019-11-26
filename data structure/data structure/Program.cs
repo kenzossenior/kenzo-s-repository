ae38c8ad8df0f42b188d599ea40c9e63c1327ed5
07KenzoSenior <07KenzoSenior@IOMG21682W>
2019-11-26 15:42:57 +0000

    writing problem

--- data structure/data structure/Program.cs
+++ data structure/data structure/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace data_structure
 {
@@ -11,11 +12,33 @@ namespace data_structure
         static void Main(string[] args)
         {
             pupilInformation[] studentData = new pupilInformation[20];
-            int num;
-            GetPupilInfo(studentData, out num);
-            DisplayPupilInfo(studentData, num);
+            int num = 0;
+            int choice = 0;
+
+            while (choice != 5)
+            {
+                choice = Menu();
+                if (choice == 1)
+                {
+                    GetPupilInfo(studentData, out num);
+                }
+                else if (choice == 2)
+                {
+                    loadData(num, studentData);
+                }
+                else if (choice == 3)
+                {
+                    writeData(num, studentData);
+                }
+                else if (choice == 4)
+                {
+                    DisplayPupilInfo(studentData, num);
+                }
+            }
             Console.ReadKey();
         }
+
+
 
         public struct pupilInformation
         {
@@ -31,7 +54,7 @@ namespace data_structure
 
             Console.WriteLine("Enter students in class:");
             int.TryParse(Console.ReadLine(), out num);
-            for (int i = 0; i < num; i ++)
+            for (int i = 0; i < num; i++)
             {
                 Console.WriteLine("Enter firstname of student {0}:", i + 1);
                 studentData[i].firstName = Console.ReadLine();
@@ -41,7 +64,7 @@ namespace data_structure
                 studentData[i].school = Console.ReadLine();
                 Console.WriteLine("Enter testscore of student {0}:", i + 1);
                 studentData[i].testScore = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter current grade of student {0}:", i + 1);
+                Console.WriteLine("Enter current grade of student {0} (A - U):", i + 1);
                 studentData[i].currentGrade = char.Parse(Console.ReadLine());
             }
         }
@@ -52,13 +75,61 @@ namespace data_structure
             {
                 Console.WriteLine("Student: {0}", i + 1);
                 Console.WriteLine("Name: {0} {1}", studentData[i].firstName, studentData[i].surname);
-                Console.WriteLine("Testscore: {0}", studentData[i].school);
+                Console.WriteLine("School: {0}", studentData[i].school);
                 Console.WriteLine("Testscore: {0}", studentData[i].testScore);
                 Console.WriteLine("Current grade: {0}", studentData[i].currentGrade);
 
             }
         }
-    }
 
+        static void loadData(int num, pupilInformation[] studentData)
+        {
+            using (StreamWriter CurrentFile = new StreamWriter("studentData.bin"))
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    CurrentFile.WriteLine("{0}", studentData[i].firstName);
+                    CurrentFile.WriteLine("{0}", studentData[i].surname);
+                    CurrentFile.WriteLine("{0}", studentData[i].school);
+                    CurrentFile.WriteLine("{0}", studentData[i].testScore);
+                    CurrentFile.WriteLine("{0}", studentData[i].currentGrade);
+                }
+            }
+            Console.WriteLine("loaded into program");
+        }
+
+        static void writeData(int num, pupilInformation[] studentData)
+        {
+            using (StreamReader CurrentFile = new StreamReader("studentData.bin"))
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    studentData[i].firstName = CurrentFile.ReadLine();
+                    studentData[i].surname = CurrentFile.ReadLine();
+                    studentData[i].school = CurrentFile.ReadLine();
+                    studentData[i].testScore = int.Parse(CurrentFile.ReadLine());
+                    studentData[i].currentGrade = char.Parse(CurrentFile.ReadLine());
+                }
+                Console.WriteLine("Written to file");
+            }
+        }
+
+        public static int Menu()
+        {
+            int input;
+            Console.WriteLine("Would you like to:");
+            Console.WriteLine("1) Enter pupil details");
+            Console.WriteLine("2) Load from file to program");
+            Console.WriteLine("3) write to file");
+            Console.WriteLine("4) Display information");
+            Console.WriteLine("5) exit program");
+            int.TryParse(Console.ReadLine(), out input);
+
+            return input;
+
+        }
+    }
 }
+
+    
 
