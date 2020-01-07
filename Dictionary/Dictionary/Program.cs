3f66923473922648ee59386ba9b4dfa0e9a7c4e9
07KenzoSenior <07KenzoSenior@IOMG21682W>
2020-01-07 14:04:55 +0000

    bruh

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> Dictionary = new Dictionary<string, int>();
+            string filename = "ah.txt";
+            try
+            {
+                using (StreamReader currentfile = new StreamReader(filename))
+                {
+                    do
+                    {
+                        CountWords(currentfile.ReadLine().ToLower(), Dictionary);
+                    } while (currentfile.Peek() != -1);
+
+                    outputWordCount(Dictionary);
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("File not found");
+                throw;
+            }
+
+                Console.ReadKey();
+        }
+
+        static void CountWords(string sentence, Dictionary<string, int> Dictionary)
+        {
+            string[] separators = { ".", ",", "!", ";", ":", "?" };
+            string[] wordArray = sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(var word in wordArray)
+            {
+                if (Dictionary.ContainsKey(word))
+                {
+                    Dictionary[word] = Dictionary[word] + 1;
+                }
+                else
+                {
+                    Dictionary.Add(word, 1);
+                }
+            }
+        }
+
+        static void outputWordCount(Dictionary<string, int> Dictionary)
+        {
+            foreach (KeyValuePair<string, int> definition in Dictionary)
+            {
+                Console.WriteLine(definition.Key + " " + definition.Value);
+            }
+        }
+    }
+}
---
+++ HashSet/HashSet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> someNumbers = new HashSet<int>() { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(someNumbers.Count);
+            Console.WriteLine("Adding the number 12");
+            someNumbers.Add(12);
+            Console.WriteLine(someNumbers.Count);
+            Console.WriteLine("aad now adding the numbers 4 and 5");
+            someNumbers.Add(5);
+            someNumbers.Add(4);
+            Console.WriteLine(someNumbers.Count);
+            Console.WriteLine("Adding the number 7");
+            someNumbers.Add(7);
+            Console.WriteLine(someNumbers.Count);
+
+            foreach (int val in someNumbers)
+            {
+                Console.WriteLine(val);
+            }
+            Console.ReadKey();
+        }
+    }
+}
