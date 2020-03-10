4b4240c9e33744bd0b9d57b03204343470f590a0
07KenzoSenior <07KenzoSenior@IOMG21682W>
2020-03-10 15:46:01 +0000

    yuh

---
+++ OOP_Ex2/OOP_Ex2/CarClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Ex2
+{
+    class CarClass
+    {
+        private string make;
+        private string model;
+        private int mileage;
+        private string colour;
+        private int age;
+
+        public Car(string make, string model, int mileage,string colour, int age)
+        {
+            this.make = make;
+            this.model = model;
+            this.mileage = mileage;
+            this.colour = colour;
+            this.age = age;
+        }
+        public DisplayDetails()
+        {
+
+        }
+
+        public ChangeMileage()
+        {
+
+        }
+    }
+}
---
+++ OOP_Ex2/OOP_Ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarClass Auto1 = new CarClass("Ford ", "Escort", 250000, "silver", 25);
+            CarClass Auto2 = new CarClass("Hyundai ", "i20", 32000, "silver", 25);
+            CarClass Auto3 = new CarClass("Ford ", "Escort", 250000, "silver", 25);
+
+        }
+
+    }
+}    
+
