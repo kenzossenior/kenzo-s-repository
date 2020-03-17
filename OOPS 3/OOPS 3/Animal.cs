8cda9d27d55283addca4ca9f3a9571ae2a2deb0e
07KenzoSenior <07KenzoSenior@IOMG21682W>
2020-03-17 15:42:19 +0000

    yuh

---
+++ OOPS 3/OOPS 3/Animal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_3
+{
+    class Animal
+    {
+        // variables
+        private string name;
+        private string gender;
+
+        public string Name { get { return this.name; } }
+        public string Gender { get { return this.gender; } }
+
+        public Animal(string name)
+        {
+            SetGender();
+            NameAnimal(name);
+        }
+
+        public Animal()
+        {
+            SetGender();
+        }
+
+        public void NameAnimal(string name)
+        {
+            this.name = name;
+        }
+
+        virtual public string makeNoise()
+        {
+            return "Gurgle";
+        }
+
+        public void SetGender()
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            if (rnd.Next(0, 2) == 0)
+            {
+                this.gender = "male";
+            }
+            else { this.gender = "female"; }
+        }
+
+    }
+
+    class Dog : Animal
+    {
+        public Dog()
+            : base()
+        { }
+
+        public Dog(string name)
+                   : base(name)
+        { }
+
+        override public string makeNoise()
+        {
+            return "woof";
+        }
+    }
+
+    class Cat : Animal
+    {
+        public Cat()
+            : base()
+        { }
+
+        public Cat(string name)
+           : base(name)
+        { }
+        override public string makeNoise()
+        {
+            return "meow";
+        }
+        public string nameAnimal()
+        {
+            Console.WriteLine("What is the name of your dog?");
+            this.name = Console.ReadLine();
+            return this.name;
+        }
+    }
+}
