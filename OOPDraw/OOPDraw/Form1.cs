b313c8296d18545918d3befe393cfce6c6e12369
skenz <skenz@DESKTOP-VPM3HLP>
2020-03-30 15:11:56 +0100

    TEST - 20200330a

--- OOPDraw/OOPDraw/Form1.cs
+++ OOPDraw/OOPDraw/Form1.cs
@@ -26,7 +26,7 @@ namespace OOPDraw
 
             float turtleX = e.X - Width / 2 + 8;
             float turtleY = Height / 2 - e.Y - 19;
-            string SelectedItem = (string)comboBox1.SelectedItem;
+            string SelectedItem = (string)comboBox1.SelectedItem; TEST
             if (SelectedItem == "Draw Triangle")
             {
                 var tri = new EqualateralTriangle(turtleX, turtleY, 50);
