3513d58553e43f86b52aa6bec575facdb37a8d7f
skenz <skenz@DESKTOP-VPM3HLP>
2020-04-27 22:44:42 +0100

    bits

--- Lockdown L1/Lockdown L1/Program.cs
+++ Lockdown L1/Lockdown L1/Program.cs
@@ -10,6 +10,20 @@ namespace Lockdown_L1
     {
         static void Main(string[] args)
         {
+            Deck cards = new Deck();
+            Console.WriteLine("DEAL 7 CARDS:");
+            cards.OutputCards(7);
+
+            Console.WriteLine("Cut the deck, then deal 7 more cards ");
+            cards.CutDeck();
+            cards.OutputCards(7);
+
+            Console.WriteLine("Shuffle the deck, then deal a further 7 cards");
+            cards.Shuffle();
+            cards.OutputCards(7);
+
+            Console.ReadKey();
+            
         }
     }
 }
