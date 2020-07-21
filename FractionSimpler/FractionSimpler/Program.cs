32b9f5464abb4b89b8da6caa5c4d381403e2563b
07KenzoSenior <07KenzoSenior@IOMG21682W>
2020-07-21 13:49:03 +0100

    done

---
+++ FractionSimpler/FractionSimpler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionSimpler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Numerator of fraction");
+            int.TryParse(Console.ReadLine(), out int top);
+            Console.WriteLine("Enter denominator of fraction");
+            int.TryParse(Console.ReadLine(), out int Bottom);
+
+            bool check = false;
+            int hcf = 1;
+
+            for (int i = top; i > 0; i--)
+            {
+                if (check == false && Bottom % i == 0 && top % i == 0)
+                {
+                     hcf = i;
+                    check = true;
+                }
+            }
+            Console.WriteLine($"{(top / hcf)}/{ Bottom / hcf}");
+            Console.ReadKey();
+
+        }
+    }
+}
