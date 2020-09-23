012b73dfc2032dec7dea61ae4244bc06b598ffcd
skenz <skenz@DESKTOP-N0I0GRT>
2020-09-23 09:39:03 +0100

    choices done

---
+++ tutorialTextBasedAdventureGame/tutorialTextBasedAdventureGame/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tutorialTextBasedAdventureGame
+{
+    public static class Game
+    {
+        static String CharacterName;
+
+        public static void StartGame()
+        {
+            Dialog("Adventure Game");
+            Dialog("Press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+            NameCharacter();
+            Console.Clear();
+
+            Console.WriteLine("Are you gay? A = Yes B = Yes");
+            Choice();
+        }
+        public static void NameCharacter()
+        {
+            Console.WriteLine("Enter Character Name");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine($"Character name: {CharacterName}");
+        }
+
+        static void Dialog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void Dialog(string message, string colour)
+        {
+            if (colour == "red")
+            { Console.ForegroundColor = ConsoleColor.Red; }
+            else if (colour == "green")
+            { Console.ForegroundColor = ConsoleColor.Green; }
+            else if (colour == "yellow")
+            { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else
+            { Console.ForegroundColor = ConsoleColor.White; }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void Choice()
+        {
+            string input = "";
+            Console.WriteLine("Choice: A or B");
+            input = Console.ReadLine();
+            input.ToUpper();
+            if (input == "A")
+            {
+                Console.WriteLine("You chose A");
+            }
+            else
+            {
+                Console.WriteLine("You chose B");
+            }
+        }
+    }
+
+
+}
--- tutorialTextBasedAdventureGame/tutorialTextBasedAdventureGame/Program.cs
+++ tutorialTextBasedAdventureGame/tutorialTextBasedAdventureGame/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿/*
+ * This work is a derivative of 
+ * "C# Adventure Game" by http://programmingisfun.com, used under CC BY.
+ * https://creativecommons.org/licenses/by/4.0/
+ */
+using System;
 
 namespace tutorialTextBasedAdventureGame
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {         
+            Game.StartGame();
+            Console.ReadKey();
         }
     }
 }
